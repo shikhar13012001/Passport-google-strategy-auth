c43906d3786270d307781d7f207860bd1a6b7fd9
Shikhar <75368010+shikhar13012001@users.noreply.github.com>
2021-09-01 02:06:51 +0530

    Update keys.js
    Please use your own mongo db uri

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@ module.exports={
         clientID:"942103798248-f4kuur6t5oa0cgacpru7ds9sdtv8huv0.apps.googleusercontent.com",
         clientSecret:"oR-7Z_HZ3lf2ISKreedsqlz8"
     },
-    mongoDB:{dbURI:'mongodb+srv://Shikhar:shikhar2015@cluster0.6nqa4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'},
+    mongoDB:{dbURI: YOUR_OWN_MONGODB_URI},
     SECRET_KEY:{
         PRIVATE_KEY:'CATS_REDUX_REACT_FIREBASE'
     }

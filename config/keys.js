d4344178b0426a5104d30d59af497e5acbdbe34b
Shikhar <75368010+shikhar13012001@users.noreply.github.com>
2021-09-01 02:12:01 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 //add this to .gitignore
 module.exports={
     google:{
-        clientID:"942103798248-f4kuur6t5oa0cgacpru7ds9sdtv8huv0.apps.googleusercontent.com",
-        clientSecret:"oR-7Z_HZ3lf2ISKreedsqlz8"
+        clientID:CLIENT_ID,
+        clientSecret:CLIENT_SECRET
     },
     mongoDB:{dbURI: YOUR_OWN_MONGODB_URI},
     SECRET_KEY:{
